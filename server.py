13f81894e46128269df5e1ee94e6ab379b559059
Edson Copque <ecop@disroot.org>
2025-02-05 16:08:29 -0300

    receive_file() function fixed.

--- server.py
+++ server.py
@@ -19,18 +19,18 @@ def receive_message(sock, buffer_size=1024):
 # Function to receive files: #2:
 def receive_file(sock, file_path):
     try:
-        # Receive the file name:
+        # Receive the file name: #3:
         file_name = receive_message(sock)
         if not file_name:
             return False
         
-        # Create the 'file/' folder if it does not exist:
+        # Create the 'file/' folder if it does not exist: #4:
         os.makedirs('file', exist_ok=True)
 
-        # Path of the file where it will be saved:
+        # Path of the file where it will be saved: #5:
         file_path = os.path.join('file', file_name)
 
-        # Open the file for binary writing:
+        # Open the file for binary writing: #6:
         with open(file_path, 'wb') as file:
             while True:
                 file_data = sock.recv(1024)
@@ -44,26 +44,26 @@ def receive_file(sock, file_path):
     except Exception as error:
         print(f'Error: {error}')
 
-# Server configuration: #4:
+# Server configuration: #7:
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.bind((HOST, PORT))
 sock.listen(5)
 
 print('Server running...')
 
-# Main listening loop:
+# Main listening loop: #8:
 while True:
     try:
         newSock, _ = sock.accept()
         print('Received connection.')
 
-        # Receive and print the message:
+        # Receive and print the message: #9:
         message = receive_message(newSock)
         if message:
             print(f'Received message: {message}')
             newSock.send(b'Message received successfully.')
         
-        # Receive the file:
+        # Receive the file: #10:
         if receive_file(newSock, 'file/'):
             newSock.send(b'File received successfully.')
         newSock.close()
